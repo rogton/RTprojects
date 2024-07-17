740cd4bae457806919b88553e411e9abae1f83da
rogton <43115038+rogton@users.noreply.github.com>
2024-07-17 21:58:06 +0100

    Add files via upload

---
+++ weatherstation_07172024_RT.py
@@ -0,0 +1,509 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+"""
+This is for the indoor weather station display
+"""
+
+"""
+This display is created by Roger C and Roger D Tonneman in July 2024
+"""
+
+import digitalio
+import board
+import time
+import adafruit_bme680
+from PIL import Image, ImageDraw
+from adafruit_rgb_display import ili9341
+from adafruit_rgb_display import st7789  # pylint: disable=unused-import
+from adafruit_rgb_display import hx8357  # pylint: disable=unused-import
+from adafruit_rgb_display import st7735  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+#sensor data and conversions to English units
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+temperature_offset = -2
+tempf=(bme680.temperature + temperature_offset)*(9/5)+32
+altitude_ft=bme680.altitude*3.28
+
+BORDER = 20
+FONTSIZE = 24
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=172, height=320, x_offset=34, # 1.47" ST7789
+# disp = st7789.ST7789(spi, rotation=270, width=170, height=320, x_offset=35, # 1.9" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True, width=80,       # 0.96" MiniTFT Rev A ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, invert=True, width=80,    # 0.96" MiniTFT Rev B ST7735R
+# x_offset=26, y_offset=1,
+# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+disp = ili9341.ILI9341(
+    spi,
+    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+image = Image.new("RGB", (width, height))
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+Be sure to check the learn guides for more usage information.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+
+Author(s): Melissa LeBlanc-Williams for Adafruit Industries
+"""
+
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import subprocess
+from adafruit_rgb_display import ili9341
+from adafruit_rgb_display import st7789  # pylint: disable=unused-import
+from adafruit_rgb_display import hx8357  # pylint: disable=unused-import
+from adafruit_rgb_display import st7735  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
+import adafruit_bme680
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=172, height=320, x_offset=34, # 1.47" ST7789
+# disp = st7789.ST7789(spi, rotation=270, width=170, height=320, x_offset=35, # 1.9" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True, width=80,       # 0.96" MiniTFT Rev A ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, invert=True, width=80,    # 0.96" MiniTFT Rev B ST7735R
+# x_offset=26, y_offset=1,
+# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+disp = ili9341.ILI9341(
+    spi,
+    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+while True:
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("totality.jpeg")
+
+
+# Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+# Display image.
+    disp.image(image)
+    time.sleep(5)
+
+# Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("chicago.jpeg")
+
+# Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+# Display image.
+    disp.image(image)
+    time.sleep(5)
+
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("tday.jpeg")
+
+# Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+# Display image.
+    disp.image(image)
+    time.sleep(5)
+
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("sleigh.jpeg")
+
+# Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    disp.image(image)
+    time.sleep(5)
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("washington.jpeg")
+
+# Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    disp.image(image)
+    time.sleep(5)
+
+# display system data first time
+    image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+# First define some constants to allow easy positioning of text.
+    padding = 10
+    x = 0
+
+# Load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+    y = 0
+    draw.text((x, y), IP, font=font, fill="#FFFFFF")
+    y = 50
+    draw.text((x, y), CPU, font=font, fill="#FFFF00")
+    y = 100
+    draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+    y = 150
+    draw.text((x, y), Disk, font=font, fill="#0000FF")
+    y= 200
+    draw.text((x, y), Temp, font=font, fill="#FF00FF")
+
+    disp.image(image)
+    time.sleep(5)
+
+# VOC information goes here
+    y = 0
+    if bme680.gas < 10000:
+        print("VOC Bad!!!!!")
+        disp = ili9341.ILI9341(
+        spi,
+        rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+        )
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+        if disp.rotation % 180 == 90:
+            height = disp.width  # we swap height/width to rotate it to landscape!
+            width = disp.height
+        else:
+            width = disp.width  # we swap height/width to rotate it to landscape!
+            height = disp.height
+
+        image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
+
+# Draw a green filled box as the background
+        draw.rectangle((0, 0, width, height), fill=(250, 250, 55))
+
+# Draw a smaller inner purple rectangle
+        draw.rectangle(
+        (BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(255,8, 0)
+        )
+
+# Load a TTF Font
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+# Draw Some Text
+        text = "Warning:VOC Bad!"
+#(font_width, font_height) = font.getbbox(text)
+        draw.text(
+        (width // 2 - 145// 2, height // 2 - FONTSIZE // 2),
+        text,
+        font=font,
+        fill=(255, 255, 0),
+        )
+
+# Display image.
+        disp.image(image)
+    else:
+        disp = ili9341.ILI9341(
+        spi,
+        rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+        )
+        if disp.rotation % 180 == 90:
+            height = disp.width  # we swap height/width to rotate it to landscape!
+            width = disp.height
+        else:
+            width = disp.width  # we swap height/width to rotate it to landscape!
+            height = disp.height
+        image = Image.new("RGB", (width, height))
+
+
+# Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
+
+# Draw a green filled box as the background
+        draw.rectangle((0, 0, width, height), fill=(0, 0, 255))
+
+# Draw a smaller inner purple rectangle
+        draw.rectangle(
+        (BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(0,255, 0)
+        )
+
+# Load a TTF Font
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+# Draw Some Text
+        text = "VOC OK!"
+#(font_width, font_height) = font.getbbox(text)
+        draw.text(
+        (width // 2 - 145// 2, height // 2 - FONTSIZE // 2),
+        text,
+        font=font,
+        fill=(0, 0,0),
+        )
+
+    disp.image(image)
+    time.sleep(5)
+
+
+# Temperature goes here
+
+    image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+# First define some constants to allow easy positioning of text.
+    padding = 10
+    x = 0
+
+# Load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    #cmd = "hostname -I | cut -d' ' -f1"
+    #IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+    #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    #CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    #cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    #MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    #cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    #Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    #cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+    #Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+    y = 0
+    temp="Temperature: %0.1f  F"%  tempf
+    draw.text((x, y), temp, font=font, fill="#FFFFFF")
+    y = 50
+    Humidity="Humidity: %0.1f %%"%bme680.relative_humidity
+    draw.text((x, y), Humidity, font=font, fill="#FFFF00")
+    y = 100
+    pressure="Pressure: %0.2f hPa"% bme680.pressure
+    draw.text((x, y), pressure, font=font, fill="#00FF00")
+    y = 150
+    altitude="Altitude = %0.1f feet" % altitude_ft
+    draw.text((x, y), altitude, font=font, fill="#0055FF")
+#    draw.text((x, y), Temp, font=font, fill="#FF00FF")
+
+ #   disp.image(image)
+    time.sleep(5)
+    y = 200
+    #text("\nTemperature: %0.1f F" % (tempf))
+#text = "\nTemperature: %0.1f F" % (tempf)
+  #  print("Humidity: %0.1f %" % bme680.relative_humidity
+    #text = "Humidity: %0.1f %%" % bme680.relative_humidity
+  #  print("Pressure: %0.3f hPa" % bme680.pressure)
+#text = "Pressure: %0.3f hPa" % bme680.pressure
+  #  print("Altitude = %0.2f feet" % altitude_ft)
+
+#text = "Altitude = %0.2f feet" % altitude_ft
+
+
+    # Display image.
+    disp.image(image)
+    time.sleep(5)
